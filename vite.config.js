8c9367062744985a534f6bdd008420d16eb414be
Angga Widiarta <anggawidiarta55@gmail.com>
2024-08-01 07:10:48 +0800

    chore: update token

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
   },
   server: {
     proxy: {
-      '/api': {
-        target: 'https://ca03-mtca1ua-oc.hospitality-api.ap-sydney-1.ocs.oc-test.com',
+      "/api": {
+        target:
+          "https://ca03-mtca1ua-oc.hospitality-api.ap-sydney-1.ocs.oc-test.com",
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
+        rewrite: (path) => path.replace(/^\/api/, ""),
+        ws: true,
       },
     },
-  }
-})
+  },
+});
