768596349f129a4c643769cbf13e5a95ef394c7e
anggawidiarta <anggawidiarta55@gmail.com>
2024-08-07 19:03:35 +0800

    add pinia

--- src/main.js
+++ src/main.js
@@ -2,12 +2,15 @@ import "./assets/main.css";
 import "vue3-json-viewer/dist/index.css";
 
 import JsonViewer from "vue3-json-viewer";
-import { createApp } from "vue";
+import { createApp } from "vue"
+import { createPinia } from "pinia";
+
 import App from "./App.vue";
 import router from "./router";
 
 const app = createApp(App);
+const pinia = createPinia();
 
-app.use(router).use(JsonViewer);
+app.use(router).use(pinia).use(JsonViewer);
 
 app.mount("#app");
