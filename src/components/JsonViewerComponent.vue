701664009614e3fb2a5dc7c19952da91f8d69744
anggawidiarta <anggawidiarta55@gmail.com>
2024-08-13 20:01:47 +0800

    refactor: add jsonviewer component

---
+++ src/components/JsonViewerComponent.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { JsonViewer } from "vue3-json-viewer";
+
+const props = defineProps({
+  data: Object,
+});
+</script>
+
+<template>
+  <div v-if="props.data" class="px-5 py-3">
+    <JsonViewer
+      :value="props.data"
+      copyable
+      expandable
+      boxed
+      sort
+      theme="jv-dark"
+    />
+  </div>
+</template>
