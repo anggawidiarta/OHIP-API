845fddc2b258a94270b2f9537f80fe41c455e0f7
anggawidiarta <anggawidiarta55@gmail.com>
2024-08-24 12:24:03 +0800

    build: adjust config for shadcnui

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,16 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
@@ -18,7 +25,6 @@ export default defineConfig({
           "https://ca03-mtca1ua-oc.hospitality-api.ap-sydney-1.ocs.oc-test.com",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
-      
       },
     },
   },
