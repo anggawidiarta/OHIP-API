34eec7d3107999ab29838da74919e88b0535fd99
Angga Widiarta <anggawidiarta55@gmail.com>
2024-08-25 22:28:32 +0800

    chore: add room section to wrap room list

---
+++ src/components/reservation/room/RoomSection.vue
@@ -0,0 +1,13 @@
+<script setup>
+import RoomList from "./RoomList.vue";
+</script>
+
+<template>
+  <section class="py-14" id="room-list">
+    <div
+      class="px-4 mx-auto max-w-screen-xl text-gray-600 dark:text-gray-300 md:px-8"
+    >
+      <RoomList />
+    </div>
+  </section>
+</template>
