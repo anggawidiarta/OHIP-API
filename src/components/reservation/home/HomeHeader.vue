92535365b4924a151827c1b24573ae47bf0d034b
anggawidiarta <anggawidiarta55@gmail.com>
2024-08-26 16:20:28 +0800

    up

--- src/components/reservation/home/HomeHeader.vue
+++ src/components/reservation/home/HomeHeader.vue
@@ -22,13 +22,14 @@ const toggleColorMode = () => {
     id="header"
   >
     <a class="flex items-center justify-center" href="/">
-      <!-- <span class="sr-only">Luxury Hotel</span> -->
       <img
         src="/img/reserved.png"
         alt="Reservation Authorization"
         class="w-12 h-12"
       />
-      <span class="ml-2 text-2xl font-bold">Reservation Authorization</span>
+      <span class="ml-2 text-xl md:text-2xl font-bold"
+        >Reservation Authorization</span
+      >
     </a>
     <div class="flex items-center ml-auto">
       <Button variant="ghost" class="" @click="toggleColorMode">
@@ -58,16 +59,13 @@ const toggleColorMode = () => {
     class="fixed left-0 right-0 z-40 px-4 pt-6 sm:py-3 border-b top-16 md:hidden h-auto bg-background"
   >
     <Button variant="ghost" class="justify-start w-full">
-      <a href="#services">Services</a>
+      <a href="/ohip-api-test">API Testing</a>
     </Button>
     <Button variant="ghost" class="justify-start w-full">
-      <a href="#about">About</a>
+      <a href="/reservation-before">Reservation Before</a>
     </Button>
     <Button variant="ghost" class="justify-start w-full">
-      <a href="#reservation">Book Now</a>
-    </Button>
-    <Button variant="ghost" class="justify-start w-full">
-      <a href="#gallery">Gallery</a>
+      <a href="#create-reservation">Book Now</a>
     </Button>
   </nav>
 </template>
